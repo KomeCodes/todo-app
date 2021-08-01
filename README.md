cd8fe6826609d22cb7edb5ea1c8c405282aef8ba
kome lucia <luciakome@gmail.com>
2021-08-01 13:49:08 +0100

    dynamically store/remove todos in/from LS

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Frontend Mentor - Todo app Solution
 
-This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW).
+This repo contains my solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW).
+
+### Links
+
+- Live Site URL: [https://todo-app-delta-ochre.vercel.app/]
+- My solution on FrontendMentor: [https://www.frontendmentor.io/solutions/todo-app-F3cEoGFia]
 
 ## Table of contents
 
@@ -15,7 +20,7 @@ This is a solution to the [Todo app challenge on Frontend Mentor](https://www.fr
 
 ## Overview
 
-This is a simple Todo list app built with HTML, CSS and Javascript. The app has all basic functions that allows users add, delete and filter todos. Users can also drag and drop to rearrange the todos 😉
+This is a simple Todo list app built with HTML, CSS and Javascript. The app has all basic functions that allow users to add, delete, and filter todos according to status. Users can also clear all their completed tasks with a single click. This app hass a drag and drop functionality to rearrange the todos (Thanks to SortableJs library!), and oh! Their todos are in local storage so it doesn't vanish whenever they refresh the page or close their browsers. Cool right? 😉
 
 ### The challenge
 
@@ -30,18 +35,21 @@ Users should be able to:
 - Clear all completed todos
 - Toggle light and dark mode
 - **Bonus**: Drag and drop to reorder items on the list
-
-
-### Links
-
-- Solution URL: [https://www.frontendmentor.io/solutions/todo-app-F3cEoGFia]
-- Live Site URL: [https://todo-app-delta-ochre.vercel.app/]
+- **AnotherBonus**: Add and remove items to/from local storage dynamically
 
 ## My process
 
-I started by coding the design with HTML and CSS, and hardcoded the todos at first, so that i can get all the styling right. Then i took out all hardcoded todos from the page and added the functionality with Javascript that lets users create the todos. The Javascript took more time to implement and I learned a lot during the process.
+I started by coding the design with HTML and CSS, and hardcoded the todos at first, so that i can get all the styling right. Then i took out all hardcoded todos from the page and added the functionality with Javascript that lets users create the todos. The Javascript took more time to implement and I learned a lot during the process. A whole lot!
 
 As soon as everything worked fine, I refactored my code a bit then noted functionalities that I may add in the future e.g storing the todos so that they are accessible and according to date. Add a "view previous todo list that has two options, go back one step or choose the exact date of the todo list you want to view.
+
+*** A week or so later ***: Todos are now stored and removed dynamically in/from local storage. For the other features I plan to add, I wonder if creating the todos as objects, and giving them a date parameter using the date function would help. Or using the date fucntion to dynamically give each todo their IDs. I'll try all these on a later date.
+
+Right now, I am so happy with the progress I made with this challenge. It is amazing how much one can do with some knowledge of HTML, CSS, loops, conditional statements, arrays and local storage/JSON. Have you seen what I was doing with nested for loops dear??? Have you?? Hahaha!
+
+The local storage part was a bit confusing at first but once I clocked what was going on, it became really easy to manipulate. It was really fun to try several things to make something work and keep failing until you finally get it right.
+
+I know there are better ways to recreate this with less code (Actually, I hope so! 😭) and I'll keep learning and may re-do this chalenge again in the future but for now YAAASSSS girrrrrl! You rock!
 
 ### Built with
 
@@ -54,10 +62,14 @@ As soon as everything worked fine, I refactored my code a bit then noted functio
 
 I'll continue to build more websites/apps using HTML CSS and Javascript to improve my skills then learn more about APIs and then React
 
+*** Aside ***
+
+I started Javascript30 by Wes Bos and I have done the first two challenges (Not pushed it here yet). I want to complete the challenge so if you see this. Ask me about it if you don't find a repo of the project (give me some days though, please 🥺) or if you find one and it is covered in dust. Thanks my friend.
+
 ### Author
 - Frontend Mentor - @officialkome
-- Twitter - @ogheneekome
+- Twitter - @elementherlist
 
 ### Acknowledgments
-Thanks to the Frontend Mentor community for creating these challenges.
+Thanks to the Frontend Mentor community for creating these challenges. I really really enjoyed and appreciate this one! 🙏🏾
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //   Selectors
-const createTodo = document.querySelector(".create-todo");
+const todoInputEl = document.querySelector(".create-todo-field");
 const todoList = document.querySelector("#todoslist");
 const todoForm = document.querySelector("#todo-form");
 const sortTodo = document.querySelector(".todo-sort");
@@ -8,82 +8,129 @@ const activeTodosCounter = document.querySelector("#active-todo-counter");
 
 // Arrays
 
-const todos = [];
-const completedTodos = []; //Clear completed should remove all items in the todos that has the completed class and push them to the completedTodos array
+let todos = [];
+let completedTodos = [];
+
+// LOCAL STORAGE
+let todosFromLocalStorage = JSON.parse(localStorage.getItem("todos"));
+let completedTodosFromLocalStorage = JSON.parse(localStorage.getItem("completedTodos"));
+
+if (todosFromLocalStorage) {
+  todos = todosFromLocalStorage;
+  renderTodos();
+  if (completedTodosFromLocalStorage) {
+    checkCompletedTodos();
+  }
+}
 
 //Event listeners
-createTodo.addEventListener("keypress", createNewTodo);
+todoInputEl.addEventListener("keypress", createNewTodo);
 todoList.addEventListener("click", updateTodo);
 sortTodo.addEventListener("click", filterTodo);
 clearCompletedTodoBtn.addEventListener("click", clearCompletedTodo);
 
-//Functions
+//FUNCTIONS
 
 // Create new todo
 function createNewTodo(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
-    if (createTodo.value) {
-      // Div container for new todo created
-      const todo = document.createElement("li");
-      todo.classList.add("todo");
-      // todo.setAttribute("draggable", "true");
-
-      // create radio button and add to div
-      const todoCheckButton = document.createElement("input");
-      todoCheckButton.type = "radio";
-      todoCheckButton.setAttribute("name", "radio-button");
-      todoCheckButton.classList.add("check-todo");
-      todo.appendChild(todoCheckButton);
-
-      // create list item to hold the todo text and add to div
-
-      const todoInput = document.createElement("span");
-      todoInput.classList.add("todo-text");
-      todoInput.innerText = createTodo.value;
-      todo.appendChild(todoInput);
-
-      // create delete button/icon and add to div
-
-      const todoDelete = document.createElement("img");
-      todoDelete.src = "images/icon-cross.svg";
-      todoDelete.classList.add("delete-todo");
-      todo.appendChild(todoDelete);
-
-      todoList.appendChild(todo);
-      // reset create todo form to default placeholder text
+    if (todoInputEl.value) {
+      if (localStorage.getItem("todos")) {
+        todos = JSON.parse(localStorage.getItem("todos"));
+        todos.push(todoInputEl.value);
+        localStorage.setItem("todos", JSON.stringify(todos));
+      } else {
+        todos.push(todoInputEl.value);
+        localStorage.setItem("todos", JSON.stringify(todos));
+      }
       todoForm.reset();
-      todos.push(todo);
-      countTodo();
+      renderTodos();
     }
   }
+}
+
+function checkCompletedTodos() {
+  let completedTodos = JSON.parse(localStorage.getItem("completedTodos"));
+  let todosOnScreen = todoList.childNodes;
+  if (completedTodos) {
+    for (let i = 0; i < completedTodos.length; i++) {
+      todosOnScreen.forEach((element) => {
+        let todoInnerElements = element.childNodes;
+        todoInnerElements.forEach((innerElement) => {
+          if (innerElement.innerText === completedTodos[i]) {
+            innerElement.classList.add("todo-completed");
+            innerElement.previousSibling.classList.add("special-class");
+            element.classList.add("complete-div");
+            completedTodos.splice(i, 1);
+          }
+        });
+      });
+    }
+  }
+}
+
+function renderTodos() {
+  let todoItems = "";
+  for (let i = 0; i < todos.length; i++) {
+    todoItems += `<li class = "todo"><input type="radio" name="radio-button" class="check-todo"><span class ="todo-text"> ${todos[i]} </span><img src="images/icon-cross.svg" class="delete-todo-btn"></li>`;
+    todoList.innerHTML = todoItems;
+    checkCompletedTodos();
+  }
+  countTodo();
 }
 
 // Delete todo and check/uncheck todo as completed
 
 function updateTodo(e) {
+  let completedTodos = [];
   const item = e.target;
-  const todo = item.parentElement;
-  if (item.classList.contains("delete-todo")) {
-    todo.classList.add("fall");
-    todo.addEventListener("transitionend", function () {
-      todo.remove();
-    });
-    for (let i = 0; i < todos.length; i++) {
-      console.log(todos);
-      if (todos[i].classList.contains("fall")) {
-        todos.splice(i, 1);
-        i--;
-      }
+  if (item.classList.contains("delete-todo-btn")) {
+    let currentTodo = item.parentElement;
+    let currentTodoInnerContents = currentTodo.childNodes;
+    let currentTodosOnScreen = JSON.parse(localStorage.getItem("todos"));
+    completedTodos = JSON.parse(localStorage.getItem("completedTodos"));
+    for (let i = 0; i < currentTodosOnScreen.length; i++) {
+      currentTodoInnerContents.forEach((element) => {
+        // if deleted todo is a completed task, remove it from the completedTodos container in local storage so it doesn't get rendered on the screen when checkCompletedTodos() is called .... line 20
+
+        if (completedTodos) {
+          for (let j = 0; j < completedTodos.length; j++) {
+            if (
+              element.innerText === currentTodosOnScreen[i] &&
+              completedTodos[j]
+            ) {
+              completedTodos.splice(j, 1);
+              j--;
+              localStorage.setItem(
+                "completedTodos",
+                JSON.stringify(completedTodos)
+              );
+            }
+          }
+        }
+        if (element.innerText === currentTodosOnScreen[i]) {
+          currentTodosOnScreen.splice(i, 1);
+          i--;
+          currentTodo.classList.add("fall");
+          currentTodo.addEventListener("transitionend", function () {
+            currentTodo.remove();
+            localStorage.setItem("todos", JSON.stringify(currentTodosOnScreen));
+          });
+        } else {
+          return;
+        }
+      });
     }
     countTodo();
   }
 
   if (item.classList.contains("check-todo")) {
-    item.classList.toggle("special-class");
-    const todoChildren = todo.childNodes;
-    todoChildren.forEach(function (element) {
+    let todo = item.parentElement;
+    let todoItems = todo.childNodes;
+    todoItems.forEach(function (element) {
       if (element.classList.contains("todo-text")) {
+        item.classList.toggle("special-class");
         element.classList.toggle("todo-completed");
         todo.classList.toggle("complete-div");
       }
@@ -95,51 +142,106 @@ function updateTodo(e) {
 // Sort/filter through todos according to status - active, completed, all
 
 function filterTodo(e) {
+  let todos = todoList.childNodes;
   if (e.target.value === "all") {
-    for (let i = 0; i < todos.length; i++) {
-      todos[i].style.display = "grid";
-    }
+    todos.forEach((todo) => {
+      if (!todo.classList.contains("fall")) {
+        todo.style.display = "grid";
+      }
+    });
   } else if (e.target.value === "completed") {
-    for (let i = 0; i < todos.length; i++) {
-      if (todos[i].classList.contains("complete-div")) {
-        todos[i].style.display = "grid";
+    todos.forEach((todo) => {
+      if (todo.classList.contains("complete-div")) {
+        todo.style.display = "grid";
       } else {
-        todos[i].style.display = "none";
+        todo.style.display = "none";
       }
-    }
-  } else if (e.target.value === "active") {
-    for (let i = 0; i < todos.length; i++) {
-      if (!todos[i].classList.contains("complete-div")) {
-        todos[i].style.display = "grid";
+    });
+  } else {
+    todos.forEach((todo) => {
+      if (!todo.classList.contains("complete-div")) {
+        todo.style.display = "grid";
       } else {
-        todos[i].style.display = "none";
+        todo.style.display = "none";
       }
-    }
+    });
   }
 }
 
-// clear completed todos
+// clear completed todos and remove them from local storage as well
 
 function clearCompletedTodo(_event) {
-  for (let i = 0; i < todos.length; i++) {
-    console.log(todos);
-    if (todos[i].classList.contains("complete-div")) {
-      todos[i].style.display = "none";
-      todos.splice(i, 1);
-      i--;
+  // Turn childnodes to array
+  let todosItems = todoList.childNodes;
+  let htmlObjArray = [];
+  todosItems.forEach((todoItem) => {
+    htmlObjArray.push(todoItem);
+  });
+  for (let i = 0; i < htmlObjArray.length; i++) {
+    let todos = JSON.parse(localStorage.getItem("todos"));
+    let completedTodos = JSON.parse(localStorage.getItem("completedTodos"));
+    let todoItemInnerElements = htmlObjArray[i].childNodes;
+    for (let j = 0; j < todos.length; j++) {
+      todoItemInnerElements.forEach((innerElement) => {
+        if (
+          htmlObjArray[i].classList.contains("complete-div") &&
+          innerElement.innerText === todos[j]
+        ) {
+          // remove the cleared todo from the completedTodos container in local storage so it doesn't get rendered on the screen when checkCompletedTodos() is called .... line 20
+
+          for (let k = 0; k < completedTodos.length; k++) {
+            if (innerElement.innerText === completedTodos[k]) {
+              completedTodos.splice(k, 1);
+              k--;
+              localStorage.setItem(
+                "completedTodos",
+                JSON.stringify(completedTodos)
+              );
+            }
+          }
+          htmlObjArray[i].style.display = "none";
+          htmlObjArray[i].remove();
+          todos.splice(j, 1);
+          localStorage.setItem("todos", JSON.stringify(todos));
+          j--;
+        }
+      });
     }
   }
-  countTodo();
 }
 
-// Count todo and show active todo status
+// Count todos and show uncompleted todos count
 
 function countTodo() {
-  const totalTasks = todos.length;
-  const completedTasks = todos.filter((todo) =>
-    todo.classList.contains("complete-div")
-  );
-  const activeTasks = totalTasks - completedTasks.length;
+  const totalTasksContent = todoList.childNodes;
+  let totalTasks = [];
+  let completedTasks = [];
+  totalTasksContent.forEach((element) => {
+    if (!element.classList.contains("fall")) {
+      totalTasks.push(element);
+    }
+  });
+
+  totalTasks.forEach((task) => {
+    if (task.classList.contains("complete-div")) {
+      let taskElements = task.childNodes;
+      taskElements.forEach((element) => {
+        if (element.classList.contains("todo-text")) {
+          localStorage.setItem(
+            "completedTodos",
+            JSON.stringify(completedTasks)
+          );
+          completedTasks = JSON.parse(localStorage.getItem("completedTodos"));
+          completedTasks.push(element.innerText);
+          localStorage.setItem(
+            "completedTodos",
+            JSON.stringify(completedTasks)
+          );
+        }
+      });
+    }
+  });
+  const activeTasks = totalTasks.length - completedTasks.length;
   activeTodosCounter.innerText = activeTasks;
 }
 
@@ -150,29 +252,29 @@ function darkTheme() {
   if (themeToggle.indexOf("images/icon-moon.svg") != -1) {
     document.getElementById("theme-toggle").src = "images/icon-sun.svg";
     const elementsToChangeWithTheme = document.querySelectorAll(
-      ".form-group, .todos-block, .todo-sort, .create-todo, .clear-completed-btn, #todo-radio,body, header"
+      ".form-group, .todos-block, .todo-sort, .create-todo-field, .clear-completed-btn, #todo-radio,body, header"
     );
-    console.log(elementsToChangeWithTheme);
     for (let i = 0; i < elementsToChangeWithTheme.length; i++) {
       elementsToChangeWithTheme[i].classList.add("dark");
     }
   } else {
     document.getElementById("theme-toggle").src = "images/icon-moon.svg";
     const elementsToChangeWithTheme = document.querySelectorAll(
-      ".form-group, .todos-block, .todo-sort, .create-todo, .clear-completed-btn, #todo-radio,body, header"
+      ".form-group, .todos-block, .todo-sort, .create-todo-field, .clear-completed-btn, #todo-radio,body, header"
     );
-    console.log(elementsToChangeWithTheme);
     for (let i = 0; i < elementsToChangeWithTheme.length; i++) {
       setTimeout(function () {
         elementsToChangeWithTheme[i].classList.remove("dark");
         elementsToChangeWithTheme[i].classList.add("light-animation");
-      }, 30)
+      }, 30);
     }
   }
 }
-new Sortable(todoList,{
+// Drag and Drop
+
+new Sortable(todoList, {
   animation: 0,
   handle: ".todo",
   delay: 20,
-  sort: "true"
-})
+  sort: "true",
+});
